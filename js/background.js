93d026945e2093e1d82eb923c99ffd678f49984d
Felix <felix.e@gmx.de>
2020-01-09 13:10:35 +0100

    added: check if the loaded page has the correct title

--- js/background.js
+++ js/background.js
@@ -1,10 +1,23 @@
 
 const TIMER_NAME = "timer";
 
-var runRefresh = function(cfg) {
+function loadTabDetailsAfterLoad(tab, callback) {
+  chrome.tabs.query({windowId: tab.windowId, index: tab.index}, function (tabs) {
+    const ntab = tabs.pop();
+    if (ntab !== undefined && tab.status == "loading") {
+      setTimeout(function() {
+        loadTabDetailsAfterLoad(ntab, callback);
+      }, 500);
+    } else {
+      return callback(tab);
+    }    
+  });
+}
+
+function runRefresh(cfg) {
 
   // query current tab, open new tab, remove new tab
-  chrome.tabs.query({highlighted: true}, function (tabs) {
+  chrome.tabs.query({highlighted: true, currentWindow: true}, function (tabs) {
     var lastTab = tabs.pop();
 
     chrome.tabs.create({ url: cfg.url }, afterTabOpen.bind(this, lastTab));  
@@ -19,21 +32,39 @@ var runRefresh = function(cfg) {
       }, function () {});
     }
 
-    // remove new tab after 5 seconds
-    setTimeout(function () {
-      chrome.tabs.get(newTab.id, function(rtab) {
-        chrome.tabs.remove(rtab.id, function () {});
-      });
-    }, 5 * 1000);
+    loadTabDetailsAfterLoad(newTab, function (loadedTab) {
+
+      if (cfg.expectedPageTitle === '' || loadedTab.title.startsWith(cfg.expectedPageTitle)) {
+
+        // remove new tab after 5 seconds
+        setTimeout(function () {
+          chrome.tabs.get(newTab.id, function(rtab) {
+            chrome.tabs.remove(rtab.id, function () {});
+          });
+        }, 5 * 1000);
+
+      } else {
+
+        // highlight the window and show error
+        chrome.tabs.highlight({
+          windowId: newTab.windowId,
+          tabs: newTab.index
+        }, function () {
+          alert('Loaded page with title "'+loadedTab.title+'" didn\'t match expected title: "'+cfg.expectedPageTitle)+'"';
+        });    
+      }
+
+    });
 
   };
  
 };
 
 
-var refresh = function() {
+function refresh() {
   chrome.storage.local.get({
     url: 'https://www.google.com',
+    expectedPageTitle: '',
     active: false
   }, function(cfg) {
     if (cfg.active) {
@@ -42,14 +73,14 @@ var refresh = function() {
   });
 };
 
-var setTimer = function() {
+function setTimer() {
   chrome.storage.local.get({
     repeatTime: 1
   }, function (cfg) {
     chrome.alarms.clearAll(() => {
       chrome.alarms.create(TIMER_NAME, {
-        delayInMinutes: parseInt(cfg.repeatTime),
-        periodInMinutes: parseInt(cfg.repeatTime)
+        delayInMinutes: parseFloat(cfg.repeatTime),
+        periodInMinutes: parseFloat(cfg.repeatTime)
       });
     });
   });
@@ -65,6 +96,7 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
 // create timer on refresh
 chrome.storage.onChanged.addListener(function () {
   setTimer();
+  refresh();
 });
 
 // create on chrome startup
--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,12 @@
 function save_options() {
 	var inputUrl = $('#inputUrl');
 	var inputRepeatTime = $('#inputRepeatTime');
+	var inputExpectedPageTitle = $('#inputExpectedPageTitle');
 	var inputActive = $('#inputActive');
 	chrome.storage.local.set({
 		url: inputUrl.val(),
 		repeatTime: inputRepeatTime.val(),
+		expectedPageTitle: inputExpectedPageTitle.val(),
 		active: inputActive.is(':checked')
 	}, function () {});
 	window.close();
@@ -14,10 +16,12 @@ function load_options() {
   chrome.storage.local.get({
     url: 'https://www.google.com',
     repeatTime: 10,
+    expectedPageTitle: '',
     active: false
   }, function(cfg) {
     $('#inputUrl').val(cfg.url);
     $('#inputRepeatTime').val(cfg.repeatTime);
+    $('#inputExpectedPageTitle').val(cfg.expectedPageTitle);
     $('#inputActive').prop('checked', cfg.active);
   });
 }
